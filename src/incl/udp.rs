97fb7ca81340004b5448bc53ae27bea7aade114a
Nick Jason Hagen <nickjasonhagen@gmail.com>
2026-02-22 00:59:57 +0100

    udp wip

--- src/incl/udp.rs
+++ src/incl/udp.rs
@@ -4,32 +4,33 @@ use std::{net::UdpSocket };
 pub struct NscriptUDP{
     sockets: HashMap<Box<str>,UdpSocket>,
     connections: HashMap<Box<str>,std::net::SocketAddr>,
-    buffers: HashMap<Box<str>,String>
 }
 impl NscriptUDP{
    pub fn new()->NscriptUDP{
         NscriptUDP{
             sockets:HashMap::new(),
             connections:HashMap::new(),
-            buffers:HashMap::new(),
         }
     }
-    pub fn create(&mut self,port:String){
+    pub fn bind(&mut self,ip:&str, port:&str)->NscriptVar{
         // Create a UDP socket
-        let socket = UdpSocket::bind(format!("0.0.0.0:{}",&port)).expect("Failed to bind socket");
+        let socket = UdpSocket::bind(format!("{}:{}",&ip,&port)).expect("Failed to bind socket");
 
         // Set the socket to receive packets
         socket.set_nonblocking(true).unwrap();
         self.sockets.insert(port.into(), socket);
+        NscriptVar::newstring("r", format!("{}:{}",&ip,&port))
     }
-    pub fn listen(&mut self,port:&str){
-        if let Some(socket) = self.sockets.get_mut(port){
+    pub fn listen(&mut self,bindsocket:&str)->NscriptVar{
+        if let Some(socket) = self.sockets.get_mut(bindsocket){
 
             // Receive data from the client
             let mut buf = [0; 1024];
-            let (amt, src) = socket.recv_from(&mut buf).expect("Failed to receive packet");
-            self.connections.insert(format!("r{}",&port).into(), src);
-            self.buffers.insert(format!("msg{}",&port).into(), format!("{}",amt));
+            let (_, src) = socket.recv_from(&mut buf).expect("Failed to receive packet");
+            let string = String::from_utf8_lossy(&buf).to_string();
+            let addressname = format!("upd:{}",&src);
+            self.connections.insert(addressname.to_string().into(), src);
+            return  NscriptVar { name:"r".into(),stringdata:"".to_string() , stringvec:vec!(addressname,string) };
             // Handle the received data
             //println!("Received {} bytes from {}", amt, src);
 
@@ -38,5 +39,17 @@ impl NscriptUDP{
             // ...
             //socket.send_to(&resp[..], src).unwrap();
         }
+
+        NscriptVar::new("r")
+    }
+    pub fn send(&mut self,port:&str,clientsocket:&str,msg:&str)->NscriptVar{
+        if let Some(s) = self.sockets.get(port){
+            if let Some(sock) = self.connections.get(clientsocket){
+                if let Ok(e) = s.send_to(msg.as_bytes(),sock){
+                    return NscriptVar::newstring("r", format!("{}",e));
+                }
+            }
+        }
+        return NscriptVar::newstring("r", "ERROR".to_string());
     }
 }

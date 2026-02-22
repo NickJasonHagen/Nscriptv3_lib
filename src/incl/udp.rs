928f55394fda3f9936d8b8831227a195fc285bd6
Nick Jason Hagen <nickjasonhagen@gmail.com>
2026-02-22 12:52:10 +0100

    NscriptUDP binding functions

--- src/incl/udp.rs
+++ src/incl/udp.rs
@@ -20,7 +20,7 @@ impl NscriptUDP{
         socket.set_nonblocking(true).unwrap();
         self.sockets.insert(port.into(), socket);
         // return the socket object for nscript to work with
-        NscriptVar::newstring("r", format!("{}:{}",&ip,&port))
+        NscriptVar::newstring("r", format!("udp:{}:{}",&ip,&port))
     }
     pub fn listen(&mut self,bindsocket:&str)->NscriptVar{
         if let Some(socket) = self.sockets.get_mut(bindsocket){
@@ -29,7 +29,7 @@ impl NscriptUDP{
             let mut buf = [0; 1024];
             let (_, src) = socket.recv_from(&mut buf).expect("Failed to receive packet");
             let string = String::from_utf8_lossy(&buf).to_string();
-            let addressname = format!("upd:{}",&src);
+            let addressname = format!("updclient:{}",&src);
             self.connections.insert(addressname.to_string().into(), src);
             // return a nscript vector with [0] for the returning socket and [1] with the data
             return  NscriptVar { name:"r".into(),stringdata:"".to_string() , stringvec:vec!(addressname,string) };
@@ -47,4 +47,25 @@ impl NscriptUDP{
         }
         return NscriptVar::newstring("r", "ERROR".to_string());
     }
+}
+pub fn nscriptfn_udpbind(args:&Vec<&str>,block :&mut NscriptCodeBlock , storage :&mut NscriptStorage) ->NscriptVar{
+    if args.len() > 1{
+        let  ip = storage.getargstring(args[0], block);
+        let  port = storage.getargstring(args[0], block);
+        return storage.udp.bind(&ip, &port);
+
+    }else{
+        print("error: udpbind(IP,PORT) // invalid arguments given ","r");
+    }
+    return NscriptVar::new("res");
+}
+pub fn nscriptfn_udplisten(args:&Vec<&str>,block :&mut NscriptCodeBlock , storage :&mut NscriptStorage) ->NscriptVar{
+        let  socketid = storage.getargstring(args[0], block);
+        return storage.udp.listen(&socketid);
+}
+pub fn nscriptfn_udpsend(args:&Vec<&str>,block :&mut NscriptCodeBlock , storage :&mut NscriptStorage) ->NscriptVar{
+    let socketid = storage.getargstring(args[0], block);
+    let clientsocket = storage.getargstring(args[1], block);
+    let msg = storage.getargstring(args[2], block);
+    return storage.udp.send(&socketid,&clientsocket,&msg);
 }
